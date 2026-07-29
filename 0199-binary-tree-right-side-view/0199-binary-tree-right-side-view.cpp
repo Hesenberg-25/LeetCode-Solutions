a6fcb7d815e93ec5b27f071ac548650575042aac
Durvesh Patil <durveshspatil5@gmail.com>
2026-07-29 10:58:06 +0530

    Time: 0 ms (100%), Space: 15.1 MB (41.1%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -20,17 +20,15 @@ public:
 
         while(!q.empty()){
             int size=q.size();
-            vector<int> level;
 
             for (int i = 0; i < size; i++){
                 TreeNode* node=q.front();
                 q.pop();
+
+                if(i==size-1) ans.push_back(node->val);
                 if(node->left!=nullptr) q.push(node->left);
                 if(node->right!=nullptr) q.push(node->right);
-                level.push_back(node->val);
             }
-            int arrsize=level.size();
-            ans.push_back(level[arrsize-1]);
         }
         return ans;
     }

1c1fcac32cc80f7f433c57f842bb613b2f4c9316
Durvesh Patil <durveshspatil5@gmail.com>
2026-08-06 23:42:41 +0530

    Time: 0 ms (100%), Space: 95.6 MB (33.89%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,41 +1,20 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int nums1size = nums1.size();
-        int nums2size = nums2.size();
-        int ansSize = nums1size + nums2size;
-        vector<int> ans;
-        
-        int nums1counter = 0;
-        int nums2counter = 0;
-        
-        while (nums1counter < nums1size && nums2counter < nums2size) {
-            if (nums1[nums1counter] > nums2[nums2counter]) {
-                ans.push_back(nums2[nums2counter]);
-                nums2counter++;
-            } else {
-                ans.push_back(nums1[nums1counter]);
-                nums1counter++;
-            }
-        }
-        
-        while (nums1counter < nums1size) {
-            ans.push_back(nums1[nums1counter]);
-            nums1counter++;
-        }
-        
-        while (nums2counter < nums2size) {
-            ans.push_back(nums2[nums2counter]);
-            nums2counter++;
-        }
-        
+        int ansSize=nums1.size() + nums2.size();
+
+        nums1.reserve(ansSize);
+        nums1.insert(nums1.end(), nums2.begin(), nums2.end());
+
+        sort(nums1.begin(),nums1.end());
+
         double median;
         if (ansSize % 2 == 0) {
-            median = (ans[ansSize / 2 - 1] + ans[ansSize / 2]) / 2.0;
-        } else {
-            median = ans[ansSize / 2];
+            median = (nums1[ansSize / 2 - 1] + nums1[ansSize / 2]) / 2.0;
         }
-
+        else{
+            median = nums1[ansSize / 2];
+        }
         return median;
     }
 };

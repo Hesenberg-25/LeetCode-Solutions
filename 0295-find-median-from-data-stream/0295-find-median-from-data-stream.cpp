7577ff8e52f6b05b3fb9dc9c48029594b038659c
Durvesh Patil <durveshspatil5@gmail.com>
2026-09-01 23:16:21 +0530

    Time: 75 ms (53.42%), Space: 148.6 MB (30.07%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,31 +1,31 @@
 class MedianFinder {
-public:
-priority_queue<int> MaxHeap;
-priority_queue<int,vector<int>,greater<int>> MinHeap;
-    MedianFinder() {
-        
-    }
-    
-    void addNum(int num) {
-        MaxHeap.push(num); 
-        MinHeap.push(MaxHeap.top());
-        MaxHeap.pop();
+private:
+    priority_queue<int> maxHeap; 
+    priority_queue<int, vector<int>, greater<int>> minHeap; 
 
-        if(MinHeap.size()>MaxHeap.size()){
-            MaxHeap.push(MinHeap.top());
-            MinHeap.pop();
-        }  
+public:
+    MedianFinder() {}
+
+    void addNum(int num) {
+        if (maxHeap.empty() || num <= maxHeap.top()) {
+            maxHeap.push(num);
+        } else {
+            minHeap.push(num);
+        }
+
+        if (maxHeap.size() > minHeap.size() + 1) {
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
+        } else if (minHeap.size() > maxHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
     }
-    
+
     double findMedian() {
-        if(MaxHeap.size()>MinHeap.size()) return MaxHeap.top();
-        else return(MaxHeap.top()+MinHeap.top())/2.0;
+        if (maxHeap.size() > minHeap.size()) {
+            return maxHeap.top();
+        }
+        return (maxHeap.top() + minHeap.top()) / 2.0;
     }
 };
-
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder* obj = new MedianFinder();
- * obj->addNum(num);
- * double param_2 = obj->findMedian();
- */
